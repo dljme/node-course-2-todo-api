fb8186e42aef80fd61e6d348e9d3fbdd6b832176
Darwin Jore <>
2018-02-21 22:32:51 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(todo){
+            res.send({todo});
+        } else {
+            res.status(404).send();
+        }
+        
+    }, (err) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(3000, () => {
